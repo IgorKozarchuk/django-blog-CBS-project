01cb86fad5f4c94984bc16b9fc18b82d5d953b04
IgorKozarchuk <igor.a.kozarchuk@gmail.com>
2023-02-06 22:33:20 +0200

    authorization, new posts on top, post controls hidden if not logged in

--- blog/blog_app/views.py
+++ blog/blog_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.template.defaultfilters import slugify
 from django.urls import reverse_lazy
 from taggit.models import Tag
@@ -12,6 +13,7 @@ from .models import Post
 class BlogListView(ListView):
 	model = Post
 	template_name = "blog_app/index.html"
+	ordering = ["-date"] # sort by date in descending order (new posts on top)
 
 
 class BlogDetailView(DetailView):
@@ -30,7 +32,7 @@ def tagged(request, slug):
 	return render(request, "blog_app/index.html", context)
 
 
-class BlogCreateView(CreateView):
+class BlogCreateView(LoginRequiredMixin, CreateView):
 	model = Post
 	template_name = "blog_app/new_post.html"
 	fields = ["title", "author", "text", "img", "tags"]
@@ -41,13 +43,26 @@ class BlogCreateView(CreateView):
 		return super().form_valid(form)
 
 
-class BlogUpdateView(UpdateView):
+class BlogUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
 	model = Post
 	template_name = "blog_app/edit_post.html"
 	fields = ["title", "text", "img", "tags"]
 
+	def test_func(self):
+		obj = self.get_object() # current object returned by the view
+		return obj.author == self.request.user # author is logged in user
 
-class BlogDeleteView(DeleteView):
+
+class BlogDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
 	model = Post
 	template_name = "blog_app/delete_post.html"
 	success_url = reverse_lazy("index")
+
+	def test_func(self):
+		obj = self.get_object()
+		return obj.author == self.request.user
+
+
+# NOTE: When using mixins with class-based views the order is important.
+# LoginRequiredMixin comes first so that we force log in, then we add UserPassesTestMixin for an additional layer of functionality,
+# and finally either UpdateView or DeleteView.
