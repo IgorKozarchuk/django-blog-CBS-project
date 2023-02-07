cc8912d32a6159268693a07de13e0e765e84677a
IgorKozarchuk <igor.a.kozarchuk@gmail.com>
2023-02-07 21:16:06 +0200

    comment form if auth

--- blog/blog_app/forms.py
+++ blog/blog_app/forms.py
@@ -7,3 +7,7 @@ class CommentForm(forms.ModelForm):
 	class Meta:
 		model = Comment
 		fields = ("author", "comment")
+		widgets = {
+			"comment": forms.Textarea(attrs={"rows": 5, "cols": 40})
+		} # size of textarea field
+
